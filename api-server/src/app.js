78b41bb128ee39d77b97afde49f217d044c542f3
Ravikanth Dakarapu <ravikanth.dakarapu@gmail.com>
2019-04-06 23:43:33 +1100

    added config setup at app level

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -4,9 +4,17 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import routes from "./routes/index";
 import dbConnection from "./db/index";
+import config from "config";
 const app = express();
 
 console.log(`NODE_ENV: ${app.get("env")}`);
+
+if (!config.get("jwtPrivateKey")) {
+  // exiting the app if the jwtPrivateKey variable is not set
+  // exit code other than zero indicates error
+  console.log("FATAL ERROR: jwtPrivateKey is not defined!!");
+  process.exit(1);
+}
 
 switch (process.env.NODE_ENV) {
   case process.env.NODE_ENV:
--- api-server/src/controllers/authController.js
+++ api-server/src/controllers/authController.js
@@ -6,15 +6,21 @@ import {
   searchAndUpdateUser,
   deleteUser
 } from "../db/models/user";
+import config from "config";
 
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function validateUser(pwd, email) {
   let user = await getUserByEmail(email);
   if (user && !user.hasOwnProperty("message")) {
     let validUser = await bcrypt.compare(pwd, user.password);
     if (validUser) {
-      return user;
+      let token = jwt.sign(
+        { _id: user._id, firstName: user.firstName, email: user.email },
+        config.get("jwtPrivateKey") // reading this parameter from environment variable
+      );
+      return token;
     }
   }
   return null;
